c2731858a58bca8480ec429a7cd99c2b79684f6b
freedombridget <freedombridget0@gmail.com>
2022-06-02 04:28:20 -0700

    my commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main: print the string in the input function
+ *
+ * decription:using the main function
+ * this print "programming is like building a multilingual puzzle"
+ * return: 0
+ */
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
