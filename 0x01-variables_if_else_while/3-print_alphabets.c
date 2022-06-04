dd8b3f88b94d12b035dde8ef8885bdc122338b7d
freedombridget <freedombridget0@gmail.com>
2022-06-03 18:26:06 -0700

    my commit

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,11 +12,12 @@ int main(void)
 {
 	int ch;
 
-	for (ch = 'a'; ch <= 'Z'; ch++)
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		putchar(ch);
 	}
-
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
 	putchar('\n');
 
 	return (0);
