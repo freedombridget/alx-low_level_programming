c7c02f62622a342347f450545bbd64443769ae76
freedombridget <freedombridget0@gmail.com>
2022-06-03 18:50:56 -0700

    my commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Description: prints the lowercase alphabet in reverse
+ * starting with z, followed by a new line.
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int firstLetter, n;
+
+	for (firstLetter = 122, n = 1; n < 27; firstLetter--, n++)
+	
+	putchar(10);
+	return (0);
+}
