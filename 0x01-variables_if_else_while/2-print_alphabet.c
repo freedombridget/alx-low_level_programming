58550afd6997f50d5b492f4317ad4a443fb96f27
freedombridget <freedombridget0@gmail.com>
2022-06-03 19:27:04 -0700

    my commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int ch;
+
+	/* print in lower letter */
+
+	for (ch = 'a'; ch <= 'z'; c++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+
+	return (0);
+}	
