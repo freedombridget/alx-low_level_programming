5211226fa367d8b78cc61b2047d89e5ecb1b1659
freedombridget <freedombridget0@gmail.com>
2022-06-07 13:45:11 -0700

    my commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints _putchar, followed by a new line
+ * and return 0
+ *
+ * * Return: Always 0.
+ */
+int main(void)
+{
+	int n;
+
+	if (n == _putchar)
+	{
+		print(_putchar '\n');
+	}
+
+	return (0);
+}
