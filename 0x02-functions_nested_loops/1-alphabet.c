370063aca7e444a2834e80bc826b4e3a4b1f4411
freedombridget <freedombridget0@gmail.com>
2022-06-07 14:20:05 -0700

    my commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,19 @@
-#include "main.h"
+/*
+ *
+ * File: 2-print_alphabet.c
+ *
+ * Auth: Brennan D Baraban
+ *
+ */
+
+#include <stdio.h>
 
 /**
  * main - Prints the alphabet in lowercase.
- *
+ 
  * Return: Always 0.
  */
+
 int main(void)
 {
 	char letter;
@@ -12,7 +21,9 @@ int main(void)
 	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
 
+
 	putchar('\n');
+
 
 	return (0);
 }
