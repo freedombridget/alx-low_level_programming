2a396a8e796332ac1cb196c9de02d9d6eac3e9e0
freedombridget <freedombridget0@gmail.com>
2022-06-08 10:43:37 -0700

    my commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,11 +7,11 @@
  */
 void print_alphabet(void)
 {
-        char alpha;
+	char alpha;
 
 	for (alpha = 'a'; alpha <= 'z'; alpha++)
 	{
-	        _putchar (alpha);
+		_putchar (alpha);
 	}
 	_putchar('\n');
 }
