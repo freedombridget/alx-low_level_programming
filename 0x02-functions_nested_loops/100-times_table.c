6c000c322fb393f7275e9dfe7736f10c081eeeec
freedombridget <freedombridget0@gmail.com>
2022-06-08 20:49:18 -0700

    my commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,51 +1,51 @@
 #include "main.h"
+
 /**
- * print_times_table  -prints tables
- * @n: print int
- * Return: nothing
+ * print_times_table - Prints a multiplication table up to param
+ * @n: The number to be treated
+ *
+ * Return: Number matrix
  */
 void print_times_table(int n)
 {
-	int a, b, c;
+	int x, y, z;
 
-	if (n < 16 && n > -1)
+	if (n >= 0 && n <= 14)
 	{
-		for (a = 0; a <= n; a++)
+		for (x = 0; x <= n; x++)
 		{
-			for (b = 0; b <= n; b++)
+			for (y = 0; y <= n; y++)
 			{
-				c = a * b;
-				if ((c / 10) == 0)
+				z = x * y;
+				if (z > 99)
 				{
-					if  (b == 0)
-					{
-						_putchar ('0');
-					}
-					if (b != 0)
-					{
-						_putchar (' ');
-						_putchar (' ');
-						_putchar ((c % 10) + '0');
-					}
+					_putchar(',');
+					_putchar(32);
+					_putchar((z / 100) + '0');
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
 				}
-				else if ((c / 10) <= 9 && ((c % 10) <= 9))
+				else if (z > 9)
 				{
-					_putchar (' ');
-					_putchar ((c / 10) + '0');
-					_putchar ((c % 10) + '0');
+					_putchar(',');
+					_putchar(32);
+					_putchar(32);
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
 				}
 				else
 				{
-					_putchar ((c / 100) + '0');
-					_putchar (((c / 10) % 10)  + '0');
-					_putchar ((c % 10) + '0');
-				}
-				if (b < n)
-				{
-					_putchar(',');
-					_putchar (' ');
+					if (y != 0)
+					{
+						_putchar(',');
+						_putchar(32);
+						_putchar(32);
+						_putchar(32);
+					}
+					_putchar(z + '0');
 				}
 			}
+			_putchar('\n');
 		}
 	}
 }
