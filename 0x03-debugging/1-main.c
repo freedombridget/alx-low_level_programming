197dd6321e60d04423d571843aefbbd1a961a2b3
freedombridget <freedombridget0@gmail.com>
2022-06-09 18:20:41 -0700

    my commit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - causes an infinite loop
  *
@@ -14,11 +13,11 @@ int main(void)
 	i = 0;
 
 	/*
-	 * while (i < 10)
-	 * {
-	 * Infinite loop - No increase of variable
-	 * putchar(i);
-	 * }
+	 *while (i < 10)
+	 *{
+	 *Infinite loop - No increase of variable
+	 *putchar(i);
+	 }
 	 */
 	printf("Infinite loop avoided! \\o/\n");
 	return (0);
