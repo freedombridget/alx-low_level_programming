67c4d2c2fc08e3cc74758f0c124fe038a52296ba
freedombridget <freedombridget0@gmail.com>
2022-06-10 00:04:52 -0700

    my commit

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,7 +8,7 @@
  */
 void print_line(int n)
 {
-	int co;
+	int i;
 
 	if (n <= 0)
 	{
@@ -16,7 +16,7 @@ void print_line(int n)
 	}
 	else
 	{
-		for (co = 1; co <= n; co++)
+		for (i = 1; i <= n; i++)
 		{
 			_putchar('_');
 		}
