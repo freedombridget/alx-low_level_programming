99edf3ba3560b4f446559568e54562f597119d62
freedombridget <freedombridget0@gmail.com>
2022-06-13 13:08:30 -0700

    my commit meassage

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string,
+ * @str: A pointer to an int that will be updated
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int x; /*Declaring variables*/
+
+	for (x = 0; str[x] != '\0'; x++) /*Star FOR*/
+	{
+		if (x % 2 == 0)
+		_putchar (str[x]); /*display contents*/	
+	}
+
+	_putchar ('\n');
+}
