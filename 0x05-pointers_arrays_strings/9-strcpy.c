a794d55267e399201b577df0f79581ff41e5224d
freedombridget <freedombridget0@gmail.com>
2022-06-13 14:02:40 -0700

    my commit message

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strcpy -  copies the string pointed
+ * @dest: A pointer to an char that will be updated
+ * @src: A pointer to an char that will be updated
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int x;
+
+	x = 0;
+	while (src[x] != '\0')
+	{
+		dest[x] = src[x];
+		x++;
+	}
+	dest[x] = '\0';
+
+	return (dest);
+}
