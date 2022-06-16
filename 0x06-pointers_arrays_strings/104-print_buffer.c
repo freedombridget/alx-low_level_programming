81e390dd1b3c478b75e1e34315d2152b69fe5c09
freedombridget <freedombridget0@gmail.com>
2022-06-16 08:44:48 -0700

    commit message

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 #include <stdio.h>
 
 /**
@@ -10,32 +10,32 @@
  */
 void print_buffer(char *b, int size)
 {
-	int byte, index;
+	int byte, i;
 
 	for (byte = 0; byte < size; byte += 10)
 	{
 		printf("%08x: ", byte);
 
-		for (index = 0; index < 10; index++)
+		for (i = 0; i < 10; i++)
 		{
-			if ((index + byte) >= size)
+			if ((i + byte) >= size)
 				printf("  ");
 
 			else
-				printf("%02x", *(b + index + byte));
+				printf("%02x", *(b + i + byte));
 
-			if ((index % 2) != 0 && index != 0)
+			if ((i % 2) != 0 && i != 0)
 				printf(" ");
 		}
 
-		for (index = 0; index < 10; index++)
+		for (i = 0; i < 10; i++)
 		{
-			if ((index + byte) >= size)
+			if ((i + byte) >= size)
 				break;
 
-			else if (*(b + index + byte) >= 31 &&
-					*(b + index + byte) <= 126)
-				printf("%c", *(b + index + byte));
+			else if (*(b + i + byte) >= 31 &&
+					*(b + i + byte) <= 126)
+				printf("%c", *(b + i + byte));
 
 			else
 				printf(".");
