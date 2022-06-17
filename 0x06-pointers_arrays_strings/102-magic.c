844f428f2645479d249bbda5235215206096666d
freedombridget <freedombridget0@gmail.com>
2022-06-16 17:53:13 -0700

    commit messade

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -22,8 +22,7 @@ int main(void)
 	 * - only one statement
 	 * - you are not allowed to code anything else than this line of code
 	 */
-	/* ...update to print 98\n */
-	int b = a[2] -= 926;
+	(p + 5) = 98;
 	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
